ef487da457ccc25b11a2e44320fc503b4c3f9fe1
SunnieHub <111681065+SunnieHub@users.noreply.github.com>
2022-11-01 18:11:37 +0700

    Update protected.lua

--- protected.lua
+++ protected.lua
@@ -2,6 +2,7 @@ if _G.key == "whitelist_397-252-480-173" then
     loadstring(game:HttpGet("https://pastebin.com/raw/5npNhNNU"))()
  elseif _G.key == "whitelist_363_932_719_620" then
     loadstring(game:HttpGet("https://pastebin.com/raw/5npNhNNU"))()
+    else
     game:GetService("StarterGui"):SetCore("SendNotification", {
   Title = "Sept hub",
   Text = "Incorrect KEY!",
